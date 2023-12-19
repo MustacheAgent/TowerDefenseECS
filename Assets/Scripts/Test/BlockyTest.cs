e943e5f5a8ae556e004842fad2099b1e972598ab
MustacheAgent <dimonus98@yandex.ru>
2023-12-19 18:13:12 +0300

    подготовка к реализации поиска в глубину

---
+++ Assets/Scripts/Test/BlockyTest.cs
@@ -0,0 +1,22 @@
+using PeartreeGames.BlockyWorldEditor;
+using UnityEngine;
+
+namespace Test
+{
+    public class BlockyTest : MonoBehaviour
+    {
+        public BlockyObjectMap map;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
