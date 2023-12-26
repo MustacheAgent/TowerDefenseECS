493ff2ab7c4594ce3ef59164764f5771752cdea1
MustacheAgent <dimonus98@yandex.ru>
2023-12-26 15:50:28 +0300

    обновление системы пути

--- Assets/Scripts/EcsStartup.cs
+++ Assets/Scripts/EcsStartup.cs
@@ -10,135 +10,132 @@ using Systems.UI;
 using UnityEngine;
 using Voody.UniLeo;
 
-namespace Scripts
+sealed class EcsStartup : MonoBehaviour
 {
-    sealed class EcsStartup : MonoBehaviour
+    private EcsWorld _world;
+    private EcsSystems _systems;
+
+    [SerializeField] private PlayerInputData inputData;
+    [SerializeField] private SceneData sceneData;
+    [SerializeField] private StaticData staticData;
+    [SerializeField] private EcsUiEmitter emitter;
+    [SerializeField] private PathfindingData pathfindingData;
+    [SerializeField] private HudData hudData;
+
+    private void Start()
     {
-        private EcsWorld _world;
-        private EcsSystems _systems;
-
-        [SerializeField] private PlayerInputData inputData;
-        [SerializeField] private SceneData sceneData;
-        [SerializeField] private StaticData staticData;
-        [SerializeField] private EcsUiEmitter emitter;
-        [SerializeField] private PathfindingData pathfindingData;
-        [SerializeField] private HudData hudData;
-
-        private void Start()
-        {
-            _world = new EcsWorld();
-            _systems = new EcsSystems(_world);
-            _systems.ConvertScene();
+        _world = new EcsWorld();
+        _systems = new EcsSystems(_world);
+        _systems.ConvertScene();
 
 #if UNITY_EDITOR
-            Leopotam.Ecs.UnityIntegration.EcsWorldObserver.Create(_world);
-            Leopotam.Ecs.UnityIntegration.EcsSystemsObserver.Create(_systems);
+        Leopotam.Ecs.UnityIntegration.EcsWorldObserver.Create(_world);
+        Leopotam.Ecs.UnityIntegration.EcsSystemsObserver.Create(_systems);
 #endif
-            //_systems.OneFrame<>
+        //_systems.OneFrame<>
 
-            AddGameplaySystems(_systems);
-            AddSpawnSystems(_systems);
-            AddTowerSystems(_systems);
-            AddMiscSystems(_systems);
+        AddGameplaySystems(_systems);
+        AddSpawnSystems(_systems);
+        AddTowerSystems(_systems);
+        AddMiscSystems(_systems);
 
-            Inject(_systems);
+        Inject(_systems);
             
-            _systems.InjectUi(emitter);
+        _systems.InjectUi(emitter);
 
-            OneFrame(_systems);
+        OneFrame(_systems);
 
-            _systems.Init();
+        _systems.Init();
 
-            // register your systems here, for example:
-            // .Add (new TestSystem1 ())
-            // .Add (new TestSystem2 ())
+        // register your systems here, for example:
+        // .Add (new TestSystem1 ())
+        // .Add (new TestSystem2 ())
 
-            // register one-frame components (order is important), for example:
-            // .OneFrame<TestComponent1> ()
-            // .OneFrame<TestComponent2> ()
+        // register one-frame components (order is important), for example:
+        // .OneFrame<TestComponent1> ()
+        // .OneFrame<TestComponent2> ()
 
-            // inject service instances here (order doesn't important), for example:
-            // .Inject (new CameraService ())
-            // .Inject (new NavMeshSupport ())
-        }
+        // inject service instances here (order doesn't important), for example:
+        // .Inject (new CameraService ())
+        // .Inject (new NavMeshSupport ())
+    }
 
-        private void AddGameplaySystems(EcsSystems systems)
+    private void AddGameplaySystems(EcsSystems systems)
+    {
+        systems
+            .Add(new PlayerInputSystem())
+            .Add(new CameraMoveSystem())
+            .Add(new TowerButtonClickSystem())
+            .Add(new ScenarioSystem())
+            .Add(new PathfindingSystem())
+            .Add(new GameProgressionSystem())
+            ;
+    }
+
+    private void AddSpawnSystems(EcsSystems systems)
+    {
+        systems
+            .Add(new EnemySpawnSystem())
+            .Add(new BuildTowerSystem())
+            .Add(new GameObjectSpawnSystem())
+            .Add(new GameObjectDestroySystem())
+            ;
+    }
+
+    private void AddTowerSystems(EcsSystems systems)
+    {
+        systems
+            .Add(new TrackTargetSystem())
+            .Add(new LaserSystem())
+            .Add(new MortarTowerSystem())
+            .Add(new MortarShellSystem())
+            .Add(new RocketTowerSystem())
+            .Add(new RocketSystem())
+            ;
+    }
+
+    private void AddMiscSystems(EcsSystems systems)
+    {
+        systems
+            .Add(new EnemyMoveSystem())
+            .Add(new DamageSystem())
+            .Add(new TimerSystem())
+            .Add(new HudUpdateSystem())
+            ;
+    }
+
+    private void Inject(EcsSystems systems)
+    {
+        systems
+            .Inject(inputData)
+            .Inject(sceneData)
+            .Inject(staticData)
+            .Inject(pathfindingData)
+            .Inject(hudData)
+            ;
+    }
+
+    private void OneFrame(EcsSystems systems)
+    {
+        systems
+            .OneFrame<CurrencyChangedEvent>()
+            .OneFrame<BaseHealthChangedEvent>()
+            ;
+    }
+
+    private void Update() 
+    {
+        _systems?.Run();
+    }
+
+    private void OnDestroy() 
+    {
+        if (_systems != null) 
         {
-            systems
-                .Add(new PlayerInputSystem())
-                .Add(new CameraMoveSystem())
-                .Add(new TowerButtonClickSystem())
-                .Add(new ScenarioSystem())
-                .Add(new PathfindingSystem())
-                .Add(new GameProgressionSystem())
-                ;
-        }
-
-        private void AddSpawnSystems(EcsSystems systems)
-        {
-            systems
-                .Add(new EnemySpawnSystem())
-                .Add(new BuildTowerSystem())
-                .Add(new GameObjectSpawnSystem())
-                .Add(new GameObjectDestroySystem())
-                ;
-        }
-
-        private void AddTowerSystems(EcsSystems systems)
-        {
-            systems
-                .Add(new TrackTargetSystem())
-                .Add(new LaserSystem())
-                .Add(new MortarTowerSystem())
-                .Add(new MortarShellSystem())
-                .Add(new RocketTowerSystem())
-                .Add(new RocketSystem())
-                ;
-        }
-
-        private void AddMiscSystems(EcsSystems systems)
-        {
-            systems
-                .Add(new EnemyMoveSystem())
-                .Add(new DamageSystem())
-                .Add(new TimerSystem())
-                .Add(new HudUpdateSystem())
-                ;
-        }
-
-        private void Inject(EcsSystems systems)
-        {
-            systems
-                .Inject(inputData)
-                .Inject(sceneData)
-                .Inject(staticData)
-                .Inject(pathfindingData)
-                .Inject(hudData)
-                ;
-        }
-
-        private void OneFrame(EcsSystems systems)
-        {
-            systems
-                .OneFrame<CurrencyChangedEvent>()
-                .OneFrame<BaseHealthChangedEvent>()
-                ;
-        }
-
-        private void Update() 
-        {
-            _systems?.Run();
-        }
-
-        private void OnDestroy() 
-        {
-            if (_systems != null) 
-            {
-                _systems.Destroy();
-                _systems = null;
-                _world.Destroy();
-                _world = null;
-            }
+            _systems.Destroy();
+            _systems = null;
+            _world.Destroy();
+            _world = null;
         }
     }
 }
---
+++ Assets/Scripts/Systems/Core/AStarPathfindingSystem.cs
@@ -0,0 +1,450 @@
+﻿using Components;
+using Components.Core;
+using Leopotam.Ecs;
+using UnityEngine;
+using Unity.Mathematics;
+using System.Collections.Generic;
+using Enums;
+using Scripts;
+using Events.Enemies;
+using Services;
+
+namespace Systems.Core
+{
+    public class AStarPathfindingSystem : IEcsInitSystem, IEcsRunSystem
+    {
+        private readonly EcsFilter<PositionComponent, PathComponent, FindPathEvent> _units = null;
+        private readonly PathfindingData _pathfindingData = null;
+
+        private readonly EcsWorld _world = null;
+
+        private const int MOVE_STRAIGHT_COST = 10;
+        private const int MOVE_DIAGONAL_COST = 14;
+
+        public void Init()
+        {
+            //SetGridSize();
+            CreateGrid();
+            //SetInitialState();
+            //SetPathfindingValues(_sceneData.tiles);
+        }
+
+        public void Run()
+        {
+            foreach(var i in _units)
+            {
+                _units.GetEntity(i).Del<FindPathEvent>();
+                ref var unitPosition = ref _units.Get1(i);
+                var tilePosition = NodeFromWorldPointUnsafe(unitPosition.transform.position);
+                ref var unitPath = ref _units.Get2(i);
+                var path = FindPath(tilePosition, _pathfindingData.Destination);
+                if (path != null)
+                {
+                    unitPath.Path = new List<int2>(path);
+                    unitPath.PathIndex = 1;
+                    var currentPathXY = unitPath.Path[unitPath.PathIndex];
+                    var index = 
+                        PathfindingExtensions.CalculateIndex(currentPathXY.x, currentPathXY.y, _pathfindingData.gridSizeX);
+                    unitPath.CurrentDestination =
+                        _pathfindingData.Tiles[index].Get<PositionComponent>().transform.position;
+                }
+            }
+        }
+
+        private List<int2> FindPath(int2 start, int2 end)
+        {
+            var startNodeIndex = PathfindingExtensions.CalculateIndex(start.x, start.y, _pathfindingData.gridSizeX);
+            var endNodeIndex = PathfindingExtensions.CalculateIndex(end.x, end.y, _pathfindingData.gridSizeX);
+            /*
+            int startNodeIndex = NodeFromPoint(start);
+            int endNodeIndex = NodeFromPoint(end);
+            */
+
+            EcsEntity[] tiles = new EcsEntity[_pathfindingData.gridSizeX * _pathfindingData.gridSizeZ];
+            _pathfindingData.Tiles.CopyTo(tiles, 0);
+            SetPathfindingValues(tiles);
+
+            EcsEntity startNode = tiles[startNodeIndex];
+            EcsEntity endNode = tiles[endNodeIndex];
+            ref var startPath = ref startNode.Get<PathfindingComponent>();
+            ref var endPath = ref endNode.Get<PathfindingComponent>();
+            startPath.gCost = 0;
+            /*
+            var neighbours = new[]
+            {
+                new int2(-1, 0),
+                new int2(+1, 0),
+                new int2(0, -1),
+                new int2(0, +1),
+                new int2(-1, -1),
+                new int2(-1, +1),
+                new int2(+1, -1),
+                new int2(+1, +1)
+            };
+            */
+            
+            int2[] neighbours = new int2[]
+            {
+                new int2(-1, 0),
+                new int2(+1, 0),
+                new int2(0, -1),
+                new int2(0, +1)
+            };
+            
+            List<int> openList = new();
+            List<int> closedList = new();
+
+            openList.Add(startPath.index);
+
+            while(openList.Count > 0)
+            {
+                int currentIndex = GetLowestFCost(openList, tiles);
+                ref var currentPath = ref tiles[currentIndex].Get<PathfindingComponent>();
+
+                if (currentIndex == endNodeIndex)
+                {
+                    // конец пути достигнут
+                    break;
+                }
+
+                openList.Remove(currentIndex);
+
+                closedList.Add(currentIndex);
+
+                for (int i = 0; i < neighbours.Length; i++)
+                {
+                    int2 neighbourOffset = neighbours[i];
+                    int2 neighbourPosition = new(currentPath.position.x + neighbourOffset.x, 
+                        currentPath.position.y + neighbourOffset.y);
+
+                    if (!IsPositionInsideGrid(neighbourPosition))
+                    {
+                        continue;
+                    }
+
+                    int neighbourIndex = PathfindingExtensions.CalculateIndex(neighbourPosition.x, neighbourPosition.y, _pathfindingData.gridSizeX);
+
+                    if (closedList.Contains(neighbourIndex))
+                    {
+                        continue;
+                    }
+
+                    ref var neighbourPath = ref tiles[neighbourIndex].Get<PathfindingComponent>();
+                    if (!neighbourPath.isWalkable)
+                    {
+                        continue;
+                    }
+
+                    int tentativeGCost = currentPath.gCost + CalculateDistanceCost(currentPath.position, neighbourPath.position);
+                    if (tentativeGCost < neighbourPath.gCost)
+                    {
+                        neighbourPath.cameFromIndex = currentIndex;
+                        neighbourPath.gCost = tentativeGCost;
+
+                        if (!openList.Contains(neighbourIndex))
+                        {
+                            openList.Add(neighbourIndex);
+                        }
+                    }
+                }
+            }
+
+            return endPath.cameFromIndex == -1 ? null : CalculatePath(endPath, tiles);
+        }
+
+        /// <summary>
+        /// Вычисляет размер сетки игрового поля.
+        /// </summary>
+        private void SetGridSize()
+        {
+            int gridSizeZ = 1;
+            int gridSizeX = 1;
+            Vector3 raycastPos = _pathfindingData.worldBottomLeft.transform.position;
+
+            // получение размера сетки по Z
+            while (true)
+            {
+                // возможно надо увеличить расстояние луча (ведь клетки не всегда будут стоят вплотную)
+                if (Physics.Raycast(raycastPos, Vector3.forward, out RaycastHit ray, 1))
+                {
+                    raycastPos = ray.collider.transform.position;
+                    gridSizeZ++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // получение размера сетки по Х
+            while (true)
+            {
+                if (Physics.Raycast(raycastPos, Vector3.right, out RaycastHit ray, 1))
+                {
+                    raycastPos = ray.collider.transform.position;
+                    gridSizeX++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            _pathfindingData.gridSizeX = gridSizeX;
+            _pathfindingData.gridSizeZ = gridSizeZ;
+        }
+
+        private void SetPathfindingValues(EcsEntity[] tiles)
+        {
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                ref var path = ref tiles[i].Get<PathfindingComponent>();
+                path.cameFromIndex = -1;
+                path.gCost = int.MaxValue;
+                path.hCost = CalculateDistanceCost(path.position, _pathfindingData.Destination);
+            }
+        }
+
+        private void CreateGrid()
+        {
+            GameObject obj = _pathfindingData.worldBottomLeft;
+
+            Vector3 first = obj.transform.position;
+            _pathfindingData.Tiles = new EcsEntity[_pathfindingData.gridSizeX * _pathfindingData.gridSizeZ];
+
+            for (var x = 0; x < _pathfindingData.gridSizeX; x++)
+            {
+                if (obj == null)
+                {
+                    var entity = _world.NewEntity();
+                    ref var path = ref entity.Get<PathfindingComponent>();
+                    path.position.x = x;
+                    path.position.y = 0;
+                    path.index = PathfindingExtensions.CalculateIndex(x, 0, _pathfindingData.gridSizeX);
+                    path.isWalkable = false;
+                    entity.Get<TileContentComponent>().content = TileContent.NonBuildable;
+                    _pathfindingData.Tiles[path.index] = entity;
+                }
+                else
+                {
+                    var entity = obj.GetEntity();
+                    ref var path = ref entity.Get<PathfindingComponent>();
+                    path.position.x = x;
+                    path.position.y = 0;
+                    path.index = PathfindingExtensions.CalculateIndex(x, 0, _pathfindingData.gridSizeX);
+
+                    if (entity.Has<TileContentComponent>() && entity.Get<TileContentComponent>().content == TileContent.SpawnPoint)
+                    {
+                        _pathfindingData.Spawn = new int2(path.position.x, path.position.y);
+                    }
+                    if (entity.Has<TileContentComponent>() && entity.Get<TileContentComponent>().content == TileContent.Destination)
+                    {
+                        _pathfindingData.Destination = new int2(path.position.x, path.position.y);
+                    }
+
+                    _pathfindingData.Tiles[path.index] = entity;
+                }
+
+                var boxCastPos = first;
+                RaycastHit hit;
+                
+                for (var z = 1; z < _pathfindingData.gridSizeZ; z++)
+                {
+                    if (Physics.BoxCast(boxCastPos, new Vector3(0.1f, 0.1f, 0.1f), Vector3.forward, out hit,
+                            _pathfindingData.worldBottomLeft.transform.rotation, 1))
+                    {
+                        var entity = hit.transform.gameObject.GetEntity();
+                        ref var path = ref entity.Get<PathfindingComponent>();
+                        path.position.x = x;
+                        path.position.y = z;
+                        path.index = PathfindingExtensions.CalculateIndex(x, z, _pathfindingData.gridSizeX);
+                        
+                        if (entity.Has<TileContentComponent>() && entity.Get<TileContentComponent>().content == TileContent.SpawnPoint)
+                        {
+                            _pathfindingData.Spawn = new int2(path.position.x, path.position.y);
+                        }
+                        if (entity.Has<TileContentComponent>() && entity.Get<TileContentComponent>().content == TileContent.Destination)
+                        {
+                            _pathfindingData.Destination = new int2(path.position.x, path.position.y);
+                        }
+                        
+                        _pathfindingData.Tiles[path.index] = entity;
+                    }
+                    else
+                    {
+                        var entity = _world.NewEntity();
+                        ref var path = ref entity.Get<PathfindingComponent>();
+                        path.position.x = x;
+                        path.position.y = z;
+                        path.index = PathfindingExtensions.CalculateIndex(x, z, _pathfindingData.gridSizeX);
+                        path.isWalkable = false;
+                        entity.Get<TileContentComponent>().content = TileContent.NonBuildable;
+                        _pathfindingData.Tiles[path.index] = entity;
+                    }
+                    
+                    boxCastPos += Vector3.forward;
+                }
+
+                if (x <= _pathfindingData.gridSizeX - 1)
+                {
+                    if (Physics.BoxCast(first, new Vector3(0.1f, 0.1f, 0.1f), Vector3.right, out hit,
+                            _pathfindingData.worldBottomLeft.transform.rotation, 1))
+                    {
+                        obj = hit.transform.gameObject;
+                        first = obj.transform.position;
+                    }
+                    else
+                    {
+                        obj = null;
+                        first += Vector3.right;
+                    }                    
+                }
+            }
+        }
+
+        private void SetInitialState()
+        {
+            GameObject obj = _pathfindingData.worldBottomLeft;
+
+            _pathfindingData.Tiles = new EcsEntity[_pathfindingData.gridSizeX * _pathfindingData.gridSizeZ];
+
+            for (int x = 0; x < _pathfindingData.gridSizeX; x++)
+            {
+                var entity = obj.GetEntity();
+                ref var path = ref entity.Get<PathfindingComponent>();
+                path.position.x = x;
+                path.position.y = 0;
+                path.index = PathfindingExtensions.CalculateIndex(x, 0, _pathfindingData.gridSizeX);
+
+                if (entity.Has<TileContentComponent>() && entity.Get<TileContentComponent>().content == Enums.TileContent.SpawnPoint)
+                {
+                    _pathfindingData.Spawn = new int2(path.position.x, path.position.y);
+                }
+                if (entity.Has<TileContentComponent>() && entity.Get<TileContentComponent>().content == Enums.TileContent.Destination)
+                {
+                    _pathfindingData.Destination = new int2(path.position.x, path.position.y);
+                }
+
+                ref var position = ref entity.Get<PositionComponent>();
+                Vector3 first;
+                var boxCastPos = first = position.transform.position;
+
+                _pathfindingData.Tiles[path.index] = entity;
+
+                RaycastHit hit;
+                for (int z = 1; z < _pathfindingData.gridSizeZ; z++)
+                {
+                    if (Physics.BoxCast(boxCastPos, new Vector3(0.1f, 0.1f, 500), Vector3.forward, out hit,
+                            _pathfindingData.worldBottomLeft.transform.rotation))
+                    {
+                        entity = hit.transform.gameObject.GetEntity();
+                        path = ref entity.Get<PathfindingComponent>();
+                        path.position.x = x;
+                        path.position.y = z;
+                        path.index = PathfindingExtensions.CalculateIndex(x, z, _pathfindingData.gridSizeX);
+
+                        _pathfindingData.Tiles[path.index] = entity;
+
+                        //position = ref entity.Get<PositionComponent>();
+                        //boxCastPos = position.transform.position;
+                    }
+                    boxCastPos += Vector3.forward;
+                }
+
+                if (Physics.BoxCast(first, new Vector3(0.1f, 0.1f, 500), Vector3.right, out hit,
+                        _pathfindingData.worldBottomLeft.transform.rotation))
+                {
+                    obj = hit.transform.gameObject;
+                }
+            }
+        }
+        
+        private List<int2> CalculatePath(PathfindingComponent endPath, EcsEntity[] tiles)
+        {
+            if (endPath.cameFromIndex == -1)
+            {
+                return null;
+            }
+            List<int2> path = new();
+            ref var currentPath = ref tiles[endPath.index].Get<PathfindingComponent>();
+            path.Add(currentPath.position);
+            
+            while (currentPath.cameFromIndex != -1)
+            {
+                ref var cameFromPath = ref tiles[currentPath.cameFromIndex].Get<PathfindingComponent>();
+                path.Add(cameFromPath.position);
+                currentPath = ref tiles[currentPath.cameFromIndex].Get<PathfindingComponent>();
+            }
+            path.Reverse();
+
+            return path;
+        }
+
+        private bool IsPositionInsideGrid(int2 position)
+        {
+            return 
+                position.x >= 0 &&
+                position.y >= 0 &&
+                position.x < _pathfindingData.gridSizeX && 
+                position.y < _pathfindingData.gridSizeZ;
+        }
+
+        private int GetLowestFCost(List<int> openList, EcsEntity[] tiles)
+        {
+            ref var lowestPath = ref tiles[openList[0]].Get<PathfindingComponent>();
+            int index = lowestPath.index;
+
+            for (int i = 1; i < openList.Count; i++)
+            {
+                ref var currentPath = ref tiles[openList[i]].Get<PathfindingComponent>();
+                if (currentPath.fCost < lowestPath.fCost)
+                {
+                    index = currentPath.index;
+                }
+            }
+
+            return index;
+        }
+
+        private int2 NodeFromPoint(Vector3 point)
+        {
+            float percentX = (point.x + (float)_pathfindingData.gridSizeX / 2) / _pathfindingData.gridSizeX;
+            float percentY = (point.z + (float)_pathfindingData.gridSizeZ / 2) / _pathfindingData.gridSizeZ;
+            percentX = Mathf.Clamp01(percentX);
+            percentY = Mathf.Clamp01(percentY);
+
+            int x = Mathf.RoundToInt((_pathfindingData.gridSizeX - 1) * percentX);
+            int y = Mathf.RoundToInt((_pathfindingData.gridSizeZ - 1) * percentY);
+
+            return new int2(x, y);
+            //return CalculateIndex(x, y, _pathfindingData.gridSizeX);
+        }
+
+        private int2 NodeFromWorldPointUnsafe(Vector3 point)
+        {
+            int x = Mathf.RoundToInt(point.x);
+            int y = Mathf.RoundToInt(point.z);
+            //Debug.Log("Point: " + point + "\n" + "Got indexes. X: " + x + " Z: " + y);
+            if (x >= 0 && x < _pathfindingData.gridSizeX && y >= 0 && y < _pathfindingData.gridSizeZ)
+            {
+                //int index = x + y * _pathfindingData.gridSizeX;
+                //Debug.Log("Index: " + index);
+                return new int2(x, y);
+            }
+            return -1;
+        }
+
+        private int CalculateDistanceCost(int2 aPos, int2 bPos)
+        {
+            int xDistance = math.abs(aPos.x - bPos.x);
+            int zDistance = math.abs(aPos.y - bPos.y);
+            int remaining = math.abs(xDistance - zDistance);
+            return MOVE_DIAGONAL_COST * math.min(xDistance, zDistance) + MOVE_STRAIGHT_COST * remaining;
+        }
+        
+        private int CalculateIndex(int x, int y, int width)
+        {
+            return x + y * width;
+        }
+    }
+}
